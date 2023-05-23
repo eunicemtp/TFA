ff47302aca818749c93e1431bd2a74c3dc847526
Eunice Mutope <120035932+eunicemtp@users.noreply.github.com>
2023-05-23 15:32:10 +0200

    acceuil mobile (bg)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,35 @@
-console.info('Hello world');
+'use strict'
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
+import { PixiPlugin } from "gsap/dist/PixiPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, MotionPathPlugin, PixiPlugin);
+
+
+
+/* /////////// */
+/* BURGER MENU */
+/* ////////// */
+
+
+var menuBurger = document.querySelector("#btn-menu");
+var menu = document.querySelector('.menu--mobile');
+menuBurger.addEventListener("click", function () {
+  menu.classList.toggle("menu--open");
+});
+
+function menuAffiche() {
+    var menuClass = "menu--open",
+        nav = document.getElementById("ancrage");
+  
+    if (nav.classList.contains(menuClass) == true) {
+      nav.classList.remove(menuClass); // Cache le menu
+    } else {
+      nav.classList.add(menuClass); // Affiche le menu
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("ancrage").addEventListener("click", menuAffiche);
+});
